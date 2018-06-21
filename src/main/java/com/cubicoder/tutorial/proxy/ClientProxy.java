9dcb9282949b8afbc68172c347172b27f8bf8dca
cubicoder <40398183+cubicoder@users.noreply.github.com>
2018-06-21 16:01:29 -0400

    Update ClientProxy.java

--- src/main/java/com/cubicoder/tutorial/proxy/ClientProxy.java
+++ src/main/java/com/cubicoder/tutorial/proxy/ClientProxy.java
@@ -4,18 +4,18 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-public class ClientProxy extends ServerProxy {
+public class ClientProxy implements IProxy {
 	
 	public void preInit(FMLPreInitializationEvent event) {
-		super.preInit(event);
+		
 	}
 
 	public void init(FMLInitializationEvent event) {
-		super.init(event);
+		
 	}
 
 	public void postInit(FMLPostInitializationEvent event) {
-		super.postInit(event);
+		
 	}
 	
 }

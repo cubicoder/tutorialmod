4ee8a080aca3892466a31ad19379aebb8e7410ff
cubicoder <40398183+cubicoder@users.noreply.github.com>
2018-06-21 16:01:54 -0400

    Update ServerProxy.java

--- src/main/java/com/cubicoder/tutorial/proxy/ServerProxy.java
+++ src/main/java/com/cubicoder/tutorial/proxy/ServerProxy.java
@@ -4,7 +4,7 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-public class ServerProxy {
+public class ServerProxy implements IProxy {
 	
 	public void preInit(FMLPreInitializationEvent event) {
 		
